acb02ccc333693882a6a4310b627f376c2ef9b04
Julio Cesar Ody <julio.ody@nexuspoint.com.au>
2010-02-18 14:15:16 +1100

    optimized

--- jquery.ondistance.js
+++ jquery.ondistance.js
@@ -1,22 +1,23 @@
 (function($) {
-  $.fn.ondistance = function(dist, close, far) {
-    var elt = this.get(0), last;
+  $.fn.ondistance = function(specified, close, far) {
+    var elt = this.get(0), 
+      last,
+      offset = $(elt).offset(),
+      center = { x: offset.left + ($(elt).width() / 2), y: offset.top + ($(elt).height() / 2) };
     $(document).mousemove(function(e) {
       // Throttle
       var current = new Date().getTime();
       if (current - last < 500) return;
       last = current;    
-      var offset = $(elt).offset(), 
-        pos = { x: offset.left+ ($(elt).width() / 2), y: offset.top + ($(elt).height() / 2) };
-      var distance = parseInt(Math.sqrt(Math.pow(e.pageX-pos.x,2) + Math.pow(e.pageY-pos.y,2)));
-      if (distance <= dist) {
-        if ($(elt).data('mouseclose')) return false;
-        close(elt);
+      var distance = parseInt(Math.sqrt(Math.pow(e.pageX-center.x, 2) + Math.pow(e.pageY-center.y, 2)));
+      if (specified >= distance) {
+        if ($(elt).data('mouseclose') == true) return false;
         $(elt).data('mouseclose', true);
+        close(elt);        
       } else {
-        if (!$(elt).data('mouseclose')) return false;
+        if ($(elt).data('mouseclose') == false) return false;
         $(elt).data('mouseclose', false);
-        far(elt);
+        far(elt);        
       }
     })
   }  
